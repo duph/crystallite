b84d196fb28803044038377a05a171b17336df51
Fredrik Persen Fostvedt <fpfostvedt@gmail.com>
2013-06-13 20:14:01 -0700

    Monkeypatched round for Numeric

--- lib/crystallite/extenders.rb
+++ lib/crystallite/extenders.rb
@@ -3,3 +3,11 @@ class Array
 		self[i], self[j] = self[j], self[i]
 	end
 end
+
+class Numeric
+	def round
+	    return (self+0.5).floor if self > 0.0
+	    return (self-0.5).ceil  if self < 0.0
+	    return 0    
+	end
+end
