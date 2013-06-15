337f2f9e4a934446df843757b64a7fbea727be16
Fredrik Persen Fostvedt <fpfostvedt@gmail.com>
2013-06-14 22:40:57 -0700

    added - crystal growth of all flakes

--- lib/crystallite/crystal.rb
+++ lib/crystallite/crystal.rb
@@ -1,24 +1,51 @@
 require_relative 'flake'
+require_relative 'helpers'
+
 
 module Crystallite
 	class Crystal
-		attr :crystal_gene
+		include Math
+		include Helpers
 
+		attr :crystal_gene
 		attr :flake_list
+		attr :polygon_list
 
 		def initialize crystal_gene
 			@crystal_gene = crystal_gene
-			@flake_list = []
 
 			grow_flakes
+			grow_crystal
 		end
 
 		def grow_flakes
+			@flake_list = []
 			@crystal_gene.each_flake_gene do |c_g|
 				@flake_list.push Crystallite::Flake.new(c_g)
 			end
 		end
 
-		
+		def grow_crystal
+			@polygon_list = []
+
+			(crystal_gene.distance_list.length-1).times do |i|
+				i += 1
+				# position flake to perimiter
+				new_points = flake_list[i].points.dup
+				new_points = move_points(new_points, [0, -crystal_gene.distance_list[i]])
+
+				# rotate flake around perimiter and copy to points
+				5.times do |i|
+					@polygon_list += [rotate_points(new_points, i*2*PI/5)]
+				end
+			end
+		end
+
+		def move position
+			# TODO: test
+			polygon_list.each do |polygon|
+				polygon = move_points(polygon, position)
+			end
+		end
 	end
 end
--- lib/crystallite/helpers.rb
+++ lib/crystallite/helpers.rb
@@ -4,8 +4,9 @@ module Crystallite
 		def move_points point_list, position
 			ret_points = []
 			point_list.reverse_each do |point|
-				ret_points.push(TMatrix.translate(position[0], position[1]))
+				ret_points.push(TMatrix.translation(position[0], position[1]) * point)
 			end
+			ret_points
 		end
 
 		def reflect_points point_list, axis
