4ec45b98db18c179940dfd56bed337387b161ffe
Fredrik Persen Fostvedt <fpfostvedt@gmail.com>
2013-06-14 19:35:57 -0700

    refactoring - removed unused variables and functions

--- lib/crystallite/flake.rb
+++ lib/crystallite/flake.rb
@@ -9,20 +9,16 @@ module Crystallite
 		attr :size
 		attr :axes
 
-		attr :new_points
-
-		attr :div_alpha, :div_radius
 		attr :angle
-		attr :sector_point_count
+		attr :radius
 
 		attr :flake_gene
 
-		def initialize size, min_points, max_points, axes, flake_gene
+		def initialize flake_gene
 			@points = []
 			@sector_points = []
 
 			@flake_gene = flake_gene
-			p flake_gene
 
 			set_growth_vars
 
@@ -32,73 +28,19 @@ module Crystallite
 			sector_points_scale
 
 			points_parse_cartesian
-			# points_sort
-			p points
 			points_replicate
-			p points
-			# generate_flake_points
 		end
-
-		def generate
-			if axes == 0
-				random_generate
-			elsif axes == 1
-				nil
-			else
-				nil
-			end
-				
-		end
-
-		def sector_points_sort
-			@sector_points.sort_by! do |point|
-				-point[1]
-			end
-		end
-
-		def points_replicate
-			@new_points = reflect_points(@points, [1,0])
-			@points += @new_points
-			@new_points = @points
-
-			(@flake_gene.axes-1).times do |i|
-				reflect_angle = -@angle - 2*i*@angle
-				@new_points = reflect_points(@new_points, [cos(reflect_angle), sin(reflect_angle)])
-				@points += @new_points
-			end
-
-			@points.each do |point|
-				point[0] += @flake_gene.size/2
-				point[1] += @flake_gene.size/2
-				point.swap! 0, 1
-			end
-		end
-
 
 		def points
 			@points
 		end
 
 
-		private
-
-		# Point evaluators
-
-		def point_acos point
-			ret = acos(point[0]/Vector[point[0],point[1]].r)
-			if ret < 0 then raise "Small" end
-			if ret > PI then raise "Large" end
-			ret
-		end
-
-		def point_radius point
-			Vector[point[0],point[1]].r
-		end
-
-
 		#
 		# Growth
 		#
+
+		private
 
 		def set_growth_vars
 			@angle = PI / @flake_gene.axes
@@ -124,10 +66,34 @@ module Crystallite
 			end
 		end
 
+		def sector_points_sort
+			@sector_points.sort_by! do |point|
+				-point[1]
+			end
+		end
+
 		def sector_points_scale
 			@sector_points.each do |point|
 				point[0] *= @radius
 				point[1] *= @angle
+			end
+		end
+
+		def points_replicate
+			new_points = reflect_points(@points, [1,0])
+			@points += new_points
+			new_points = @points
+
+			(@flake_gene.axes-1).times do |i|
+				reflect_angle = -@angle - 2*i*@angle
+				new_points = reflect_points(new_points, [cos(reflect_angle), sin(reflect_angle)])
+				@points += new_points
+			end
+
+			@points.each do |point|
+				point[0] += @flake_gene.size/2
+				point[1] += @flake_gene.size/2
+				point.swap! 0, 1
 			end
 		end
 
