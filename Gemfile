9446a8588b4a971ad9e918af1d791ebc82feb85d
Fredrik Persen Fostvedt <fpfostvedt@gmail.com>
2013-08-15 10:41:13 +0200

    final

--- Gemfile
+++ Gemfile
@@ -2,6 +2,9 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in crystallite.gemspec
 group :development do
+	gem 'rasem'
 	gem 'transformatrix'
 	gem 'hashie'
+	gem 'sinatra'
+	gem 'colorist'
 end
--- lib/crystallite.rb
+++ lib/crystallite.rb
@@ -4,34 +4,20 @@ require_relative 'crystallite/crystal'
 require_relative 'crystallite/svg_generator'
 require_relative 'crystallite/dna'
 
+include Crystallite
 
-
-width = 1000
-height = 1000
-
-f_g = Crystallite::FlakeGene.new
-c_g = Crystallite::CrystalGene.new
-
-f = Crystallite::Flake.new f_g
-c = Crystallite::Crystal.new c_g
-c.move([width/2, height/2])
-# p "sector points: #{f.sector_points}"
+require 'sinatra'
 
 
 
-svg = Crystallite::SVGGenerator.new width, height
-svg.rectangle 0,0,width,height,{ fill: Colorist::Color.from_rgb(200, 210, 255) }
-# svg.circle size/2,size/2,size/2, { fill: Colorist::Color.from_hsv(340, 0.1, 0.7) }
+get '/' do
+	svg = Crystallite::SVGGenerator.new 1000, 1000
+	svg.rectangle 0,0,1000,1000,{ fill: Colorist::Color.from_rgb(200, 210, 255) }
 
-# svg.line size,size,0,0
-# svg.line 0,size,size,0
-# svg.line size/2,0,size/2,size
-# svg.line 0,size/2,size,size/2
-
-
-# svg.polygon f.points, { fill: Colorist::Color.from_rgb(255,255,255) }
-c.polygon_list.each do |polygon_points|
-	svg.polygon polygon_points, { fill: Colorist::Color.from_rgb(255,255,255) }
+	c = Crystal.new(CrystalGene.new)
+	c.move [500,500]
+	c.polygon_list.each do |polygon_points|
+		svg.polygon polygon_points, { fill: Colorist::Color.from_rgb(255,255,255) }
+	end
+	svg.output
 end
-
-svg.write_svg '/Users/fpfostvedt/dev/gems/crystallite'
